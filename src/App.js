6e79142e0a897a9fe00d78e1becf8e73679affa0
noedurocher <noedurocher@gmail.com>
2025-08-07 15:34:42 -0400

    Clean App.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
@@ -10,37 +9,17 @@ import JobForm from './pages/JobForm';
 
 function App() {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/signup" element={<SignupForm/>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/jobForm" element={<JobForm />} />
-        </Routes>
-    </BrowserRouter>
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/signup" element={<SignupForm/>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/jobForm" element={<JobForm />} />
+            </Routes>
+        </BrowserRouter>
+
     </div>
->>>>>>> saved-work
   );
 }
 
